c79c802db648c452a9e8236db0983165659302aa
mirafzal1604 <mirvosiqovm@gmail.com>
2022-01-19 20:42:00 +0500

    second commet

---
+++ js/loader.js
@@ -0,0 +1,7 @@
+setInterval(loading, 1000);
+
+var load = document.querySelector(".loader");
+
+function loading() {
+    load.style.display = "none";
+}
--- js/nav.js
+++ js/nav.js
@@ -4,15 +4,23 @@ window.onscroll = function() {
     scrolldown();
 };
 
+const mybutton = document.getElementById("myBtn");
 const nav = document.querySelector("header");
 const logo = document.querySelector('.logo')
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
 function scrolldown() {
     if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75) {
         nav.className = "navScroll";
         logo.src = './images/logo/logo-svg.png';
+        mybutton.style.display = "block";
     } else {
         nav.className = "";
         logo.src = './images/logo/white-logo-svg.png';
+        mybutton.style.display = "none";
     }
 }
